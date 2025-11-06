45efb9f1daeb3181bc5876520d05313f977c7896
dam900 <damianjakubowski48@gmail.com>
2025-11-06 18:44:19 +0100

    added db connection to server

--- iotserver/main.py
+++ iotserver/main.py
@@ -1,5 +1,22 @@
+import os
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_PASS = os.getenv('POSTGRES_PASSWORD')
+DB_USER = os.getenv('POSTGRES_USER')
+DB_NAME = os.getenv('POSTGRES_DB')
+DB_HOST = os.getenv('POSTGRES_HOST')
+DB_PORT = os.getenv('POSTGRES_PORT')
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+engine = create_engine(DATABASE_URL)
+Base = declarative_base()
+
 def main():
-    print("Hello from iotserver!")
+    pass
 
 
 if __name__ == "__main__":
